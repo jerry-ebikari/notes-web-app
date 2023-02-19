ae57e68d3ad29b06757de67112bc672ed7835c01
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-02-19 17:04:32 +0100

    Added unstaged files from previous commit

--- src/features/core/core.module.ts
+++ src/features/core/core.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SuiSidebarModule } from '@richardlt/ng2-semantic-ui';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
-    CoreRoutingModule
-  ]
+    CoreRoutingModule,
+    SuiSidebarModule
+  ],
+  exports: [SidebarComponent]
 })
 export class CoreModule { }
