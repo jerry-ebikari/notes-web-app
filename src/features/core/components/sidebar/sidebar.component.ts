2cf89bf4deb8ccbb787392e2d7e638be1cc173b1
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-02-20 22:22:24 +0100

    Added note sidebar item

--- src/features/core/components/sidebar/sidebar.component.ts
+++ src/features/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ISidebarItem } from '../../models/sidebar-item';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  items: ISidebarItem[] = [
+    {
+      name: "Notes",
+      link: ""
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
